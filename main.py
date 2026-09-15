93049b831d46aa9d7c5328a1a0fc09d286bd4d49
Perwira Satria <114348871+PwrSat@users.noreply.github.com>
2026-09-15 17:02:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+parameters = {
+    "lat": 3.595196,
+    "lon": 98.672226,
+    "appid": os.environ["WEATHER_APP_ID"],
+    "cnt": 4,
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+will_rain = any(hour_change["weather"][0]["id"] < 700 for hour_change in data["list"])
+
+if will_rain:
+    telegram_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": "☔ Bring an umbrella, it's raining today!"
+    }
+    tg_response = requests.post(telegram_url, data=payload)
+    tg_response.raise_for_status()
+    print("Notifikasi terkirim:", tg_response.json())
